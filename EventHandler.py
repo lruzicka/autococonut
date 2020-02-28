331fd324bd413e911e5c13311c01c3933fb7a1a9
Lukáš Růžička <lruzicka@redhat.com>
2020-02-28 19:47:44 +0100

    Split functions and fix it for mouse bindings.

--- EventHandler.py
+++ EventHandler.py
@@ -1,51 +1,94 @@
 #!/usr/bin/python3
 
-from pynput.keyboard import Key
+from math import sqrt
+from pynput.keyboard import Key, KeyCode
 from pynput.keyboard import Listener as KeyListener
 from pynput.mouse import Listener as MouseListener
 
-
-class EventHandler:
-    """ Collects the events from mouse and keyboard. """
+class Handler:
     def __init__(self):
-        self.mouse = {'x':0, 'y':0, 'distance':0, 'button':'', 'scroll':0}
-        self.keyboard = {}
+        self.mx = 0
+        self.my = 0
+        self.cx = 0
+        self.cy = 0
+        self.button = ''
+        self.distance = 0
+        self.scrollx = 0
+        self.scrolly = 0
+
+    def add_to_distance(self, x, y):
+        """ Adds value to the current distance. """
+        dx = abs(self.mx - x)
+        dy = abs(self.my - y)
+        self.mx = x
+        self.my = y
+        distance = sqrt((dx**2)+(dy**2))
+        self.distance = self.distance + distance
+
+    def display_distance(self, unit=None):
+        """ Shows the reached distance in px (default), cm, or m. """
+        if unit == 'cm':
+            distance = round(self.distance * 0.026458333)
+        elif unit == 'm':
+            distance = round(self.distance * 0.00026458333)
+        else:
+            distance = self.distance
+        return distance
+
+    def add_to_scrolled(self, dx, dy):
+        """ Counts how many times a mouse has been scrolled up or down."""
+        self.scrollx += dx
+        self.scrolly += dy
+        return 0
+
+    def record_mouse_click(self, x, y, button):
+        """ Records the mouse click values for further processing."""
+        self.cx = x
+        self.cy = y
+        self.button = str(button)
+        return 0
+
+    def create_needle_coordinates(self):
+        """ Calculates needle coordinates around the click point. """
+        lx = self.cx - 20
+        rx = self.cx + 20
+        ly = self.cy - 10
+        ry = self.cy + 10
+        coordinates = (lx,ly,rx,ry)
+        return(coordinates)
+
+
+class Collector:
+    """ Collects the events from mouse and keyboard. """
+    def __init__(self, handler):
+        self.handler = handler
         with MouseListener(on_move=self.on_move, on_click=self.on_click, on_scroll=self.on_scroll) as self.mlistener, KeyListener(on_press=self.on_press, on_release=self.on_release) as self.klistener:
             self.mlistener.join()
             self.klistener.join()
+
+    def get_key_name(self, key):
+        if isinstance(key, KeyCode):
+            return key.char
+        else:
+            return str(key)
     
     def on_move(self, x, y):
-        """ Read mouse movements and track the distance of the movements. """
-        dx = abs(self.mouse['x'] - x)
-        dy = abs(self.mouse['y'] - y)
-        self.mouse['x'] = x
-        self.mouse['y'] = y
-        distance = dx + dy
-        self.mouse['distance'] = self.mouse['distance'] + distance
-        distance_m = self.mouse['distance'] * 0.00026458333
-#        print(f"{distance_m} m")
+        """ Read mouse movements. """
+        self.handler.add_to_distance(x, y)
 
     def on_click(self, x, y, button, pressed):
-        """ Read mouse clicks and record values. """
-        self.mouse['x'] = x
-        self.mouse['y'] = y
-        self.mouse['button'] = button
-        if str(button) == 'Button.right':
-            self.mouse['button'] = 'right'
-        elif str(button) == 'Button.middle':
-            self.mouse['button'] = 'middle'
-        else:
-            self.mouse['button'] = 'left'
-        print(f"Clicked at: {x},{y}, {self.mouse['button']} used.")
-        print(f"Scrolled: {self.mouse['scroll']}, total distance moved: {self.mouse['distance']}.")
+        """ Read mouse clicks. """
+        self.handler.record_mouse_click(x, y, button)
+        print(self.handler.cx, self.handler.cy, self.handler.button)
+        print(self.handler.create_needle_coordinates())
 
     def on_scroll(self, x, y, dx, dy):
         """ Read mouse scrolls and calculate the total amount of scrolls."""
-        self.mouse['scroll'] += dy 
+        self.handler.add_to_scrolled(dx, dy)
 
     def on_press(self, key):
-        """ Read keyboard presses. """
-        print(key)
+        """ Read keyboard presses and record values. """
+        print(self.get_key_name(key))
 
     def on_release(self, key):
         """ Read keyboard releases. """
@@ -55,7 +98,7 @@ class EventHandler:
             self.mlistener.stop()
             return False
 
-
-events = EventHandler()
+magicbox = Handler()
+events = Collector(magicbox)
 
 
